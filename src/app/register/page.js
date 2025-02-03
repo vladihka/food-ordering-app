c542fcdf6de137b747f2a534dc4d7e315e78f6e0
vladihka <vladihka58@gmail.com>
2025-02-03 19:10:41 +0100

    Adding register page and functionality of adding hash user to database

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,14 +1,28 @@
+"use client"
 import Image from "next/image";
+import { useState } from "react";
 
 export default function RegisterPage(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleFormSubmit(ev){
+        ev.preventDefault();
+        fetch('/api/register', {
+            method: 'POST',
+            body: JSON.stringify({email, password}),
+            headers: {'Content-Type': 'application/json'},
+        })
+    }
+
     return(
         <section className="mt-8">
             <h1 className="text-center text-primary text-4xl mb-4">
                 Register
             </h1>
-            <form className="block max-w-xs mx-auto">
-                <input type="email" placeholder="email"></input>
-                <input type="password" placeholder="password"></input>
+            <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
+                <input type="email" placeholder="email" value={email} onChange={ev=>setEmail(ev.target.value)}></input>
+                <input type="password" placeholder="password" value={password} onChange={ev=>setPassword(ev.target.value)}></input>
                 <button type="submit">Register</button>
                 <div className="my-4 text-center text-gray-500">
                     or Login with provider
