305ef099e13b4c1b2dd28b06387a3fa6e151dce9
vladihka <vladihka58@gmail.com>
2025-02-04 11:31:02 +0100

    Adding another register functionality and styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
-import Hero from "../components/layout/Hero";
-import HomeMenu from "../components/layout/HomeMenu";
-import SectionHeaders from "../components/layout/SectionHeaders";
+import Hero from "@/components/layout/Hero";
+import HomeMenu from "@/components/layout/HomeMenu";
+import SectionHeaders from "@/components/layout/SectionHeaders";
+
 
 export default function Home() {
   return (
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,18 +1,32 @@
 "use client"
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
 export default function RegisterPage(){
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [creatingUser, setCreatingUSer] = useState(false)
+    const [userCreated, setUserCreated] = useState(false)
+    const [error, setError] = useState(false)
 
-    function handleFormSubmit(ev){
+    async function handleFormSubmit(ev){
         ev.preventDefault();
-        fetch('/api/register', {
+        setCreatingUSer(true)
+        setError(false)
+        setUserCreated(false)
+        const response = await fetch('/api/register', {
             method: 'POST',
             body: JSON.stringify({email, password}),
             headers: {'Content-Type': 'application/json'},
-        })
+        });
+        if(response.ok){
+            setUserCreated(true)
+        }
+        else{
+            setError(true)
+        }
+        setCreatingUSer(false);
     }
 
     return(
@@ -20,10 +34,36 @@ export default function RegisterPage(){
             <h1 className="text-center text-primary text-4xl mb-4">
                 Register
             </h1>
+            {userCreated && (
+                <div className="my-4 text-center">User created. <br></br> Now you can{' '}
+                <Link className="underline" href={'/login'}>Login &raquo;</Link></div>
+            )}
+            {error && (
+                <div className="my-4 text-center">
+                    An error has occurred.<br></br>
+                    Please try again later.
+                </div>
+            )}
             <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
-                <input type="email" placeholder="email" value={email} onChange={ev=>setEmail(ev.target.value)}></input>
-                <input type="password" placeholder="password" value={password} onChange={ev=>setPassword(ev.target.value)}></input>
-                <button type="submit">Register</button>
+                <input 
+                    type="email" 
+                    placeholder="email" 
+                    disabled={creatingUser}
+                    value={email} 
+                    onChange={ev=>setEmail(ev.target.value)}>
+                </input>
+                <input 
+                    type="password" 
+                    disabled={creatingUser}
+                    placeholder="password" 
+                    value={password} 
+                    onChange={ev=>setPassword(ev.target.value)}>
+                </input>
+                <button 
+                    type="submit"
+                    disabled={creatingUser}>
+                    Register
+                </button>
                 <div className="my-4 text-center text-gray-500">
                     or Login with provider
                 </div>
