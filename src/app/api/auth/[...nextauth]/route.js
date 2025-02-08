17fab5dbb4ce9397071e567afaae503aff02f616
vladihka <vladihka58@gmail.com>
2025-02-09 00:08:36 +0100

    check

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,15 @@
+// auth/[...nextauth]/route.js
+
 import { User } from "@/models/User";
 import mongoose from "mongoose";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from 'bcrypt'
-import GoogleProvider from "next-auth/providers/google"
+import bcrypt from 'bcrypt';
+import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "@/libs/mongoConnect";
 
-const handler = NextAuth({
+const authOptions = {
   secret: process.env.SECRET,
   adapter: MongoDBAdapter(clientPromise),
   providers: [
@@ -30,18 +32,20 @@ const handler = NextAuth({
         const email = credentials?.email;
         const password = credentials?.password;
 
-        mongoose.connect(process.env.MONGO_URL)
-        const user = await User.findOne({email});
-        const passwordOk = user && bcrypt.compareSync(password, user.password)
+        mongoose.connect(process.env.MONGO_URL);
+        const user = await User.findOne({ email });
+        const passwordOk = user && bcrypt.compareSync(password, user.password);
 
-        if(passwordOk){
-          return user
+        if (passwordOk) {
+          return user;
         }
 
         return null;
       },
     }),
   ],
-});
+};
 
-export { handler as GET, handler as POST };
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST, authOptions };
--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
 import { User } from "@/models/User";
 import { UserInfo } from "@/models/UserInfo";
+import { authOptions } from "../auth/[...nextauth]/route";
 
 export async function PUT(req) {
     mongoose.connect(process.env.MONGO_URL)
