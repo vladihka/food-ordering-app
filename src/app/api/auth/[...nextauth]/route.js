0b652762cee9397752cb734700a4581ebf1efedf
vladihka <vladihka58@gmail.com>
2025-02-08 23:56:29 +0100

    fix

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,53 +1,23 @@
-import clientPromise from "@/libs/mongoConnect";
 import { User } from "@/models/User";
-import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import GoogleProvider from "next-auth/providers/google";
+import bcrypt from 'bcrypt'
+import GoogleProvider from "next-auth/providers/google"
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@/libs/mongoConnect";
 
 export const authOptions = {
   secret: process.env.SECRET,
-  // adapter: MongoDBAdapter(clientPromise), // Временно закомментируй
-  session: { strategy: "jwt" },
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    CredentialsProvider({
-      name: "Credentials",
-      id: "credentials",
-      credentials: {
-        email: { label: "Email", type: "email", placeholder: "test@example.com" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error("Missing email or password");
-        }
-
-        await mongoose.connect(process.env.MONGO_URL);
-        const user = await User.findOne({ email: credentials.email });
-
-        if (!user) {
-          throw new Error("User not found");
-        }
-
-        if (!bcrypt.compareSync(credentials.password, user.password)) {
-          throw new Error("Invalid credentials");
-        }
-
-        return {
-          id: user._id.toString(),
-          name: user.name,
-          email: user.email,
-        };
-      },
-    }),
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET
+  }),
   ],
-};
+adapter: MongoDBAdapter(clientPromise),
+}
 
 const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST, authOptions }
+
+export { handler as GET, handler as POST };
