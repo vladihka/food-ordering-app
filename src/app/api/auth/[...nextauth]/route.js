9e0f0d824057e3c34ea5fbaf6a60ff66c91e0ea2
vladihka <vladihka58@gmail.com>
2025-02-09 00:00:21 +0100

    check

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -6,16 +6,44 @@ import bcrypt from 'bcrypt'
 import GoogleProvider from "next-auth/providers/google"
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "@/libs/mongoConnect";
+import { debug } from "console";
 
 export const authOptions = {
   secret: process.env.SECRET,
+  adapter: MongoDBAdapter(clientPromise),
+  debug: true,
   providers: [
     GoogleProvider({
-    clientId: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET
-  }),
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      id: "credentials",
+      credentials: {
+        username: {
+          label: "Email",
+          type: "email",
+          placeholder: "test@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const email = credentials?.email;
+        const password = credentials?.password;
+
+        mongoose.connect(process.env.MONGO_URL)
+        const user = await User.findOne({email});
+        const passwordOk = user && bcrypt.compareSync(password, user.password)
+
+        if(passwordOk){
+          return user
+        }
+
+        return null;
+      },
+    }),
   ],
-adapter: MongoDBAdapter(clientPromise),
 }
 
 const handler = NextAuth(authOptions);
