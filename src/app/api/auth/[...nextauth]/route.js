0ad7df24faa2d9c446b707e215d17c6e07b1b89d
Vladyslav Semak <64697319+vladihka@users.noreply.github.com>
2025-10-02 12:56:29 +0200

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -7,7 +7,7 @@ import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "@/libs/mongoConnect";
 
-export const authOptions = {
+const authOptions = {
   secret: process.env.SECRET,
   adapter: MongoDBAdapter(clientPromise),
   providers: [
@@ -18,33 +18,4 @@ export const authOptions = {
     CredentialsProvider({
       name: "Credentials",
       id: "credentials",
-      credentials: {
-        email: {
-          label: "Email",
-          type: "email",
-          placeholder: "test@example.com",
-        },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        const email = credentials?.email;
-        const password = credentials?.password;
-
-        await mongoose.connect(process.env.MONGO_URL);
-
-        const user = await User.findOne({ email });
-
-        const passwordOk = user && bcrypt.compareSync(password, user.password);
-
-        if (passwordOk) {
-          return user;
-        }
-
-        return null;
-      },
-    }),
-  ],
-};
-
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
+      cr
