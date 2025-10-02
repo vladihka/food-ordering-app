e03aa347c10a34c836099f7665dc28b59cc2abce
Vladyslav Semak <64697319+vladihka@users.noreply.github.com>
2025-10-02 12:58:22 +0200

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -18,4 +18,31 @@ const authOptions = {
     CredentialsProvider({
       name: "Credentials",
       id: "credentials",
-      cr
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "test@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const email = credentials?.email;
+        const password = credentials?.password;
+
+        await mongoose.connect(process.env.MONGO_URL);
+
+        const user = await User.findOne({ email });
+
+        if (user && bcrypt.compareSync(password, user.password)) {
+          return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
